041d1a67c4999aaf038c563ca8723d283fe6f53f
Kevin Puig <119972216+k-puig@users.noreply.github.com>
2024-12-17 20:31:15 -0500

    new file:   .gitattributes 	new file:   .gitignore 	new file:   build.gradle 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle 	new file:   src/main/java/net/kpuig/comments/CommentsApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/net/kpuig/comments/CommentsApplicationTests.java

---
+++ src/test/java/net/kpuig/comments/CommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kpuig.comments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
